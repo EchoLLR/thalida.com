e317725cb9b8cf78ddde2580e1f043be2378c96e
Thalida Noel <thalida.c.noel@gmail.com>
2017-04-07 11:01:03 -0600

    App: Loaded in static content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+
+import { Extra, extras } from './data/extras.data';
+import { Job, jobs } from './data/jobs.data';
+import { Project, projects } from './data/projects.data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  copyrightYear: string;
+  extras: Extra[];
+  jobs: Job[];
+  projects: Project[];
+  visibleProjects: Project[];
+  featuredProject: Project;
+
+  constructor() {
+    this.copyrightYear = 'foo';
+    this.extras = extras;
+    this.jobs = jobs;
+    this.projects = projects;
+    this.visibleProjects = _.filter(this.projects, {hidden: false});
+
+    let featuredProjs = _.filter(this.projects, {featured: true});
+    if (featuredProjs.length > 0) {
+      this.featuredProject = featuredProjs[0];
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
+import * as _ from 'lodash';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -18,6 +21,7 @@ import { HighlightComponent } from './components/highlight/highlight.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     MaterialModule
